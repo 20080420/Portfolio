401c39c36e01fb5386972aaec9e5f85729a48fd5
20080420 <135636204+20080420@users.noreply.github.com>
2023-11-06 23:43:19 +0800

    Update Mastermind.py

--- Mastermind.py
+++ Mastermind.py
@@ -1,118 +1,109 @@
 import random
-
-#starting screen
-def intro():
-  print("++++++++++++++++++++++++++++++++++++++++")
-  print("+        Master Mind with Fruits       +")
-  print("+         made by Rachel Foo           +")
-  print("++++++++++++++++++++++++++++++++++++++++")
-  print("+               Tutorial               +")
-  print("+ 1)The game will generate 4 fruits    +")
-  print("+   From the list of apple, banana,    +")
-  print("+   orange, and lemon.                 +")
-  print("+                                      +")
-  print("+ 2)The user will have to guess which  +")
-  print("+   fruits are in which slot           +")
-  print("+                                      +")
-  print("+ 3)After each guess, the game will    +")
-  print("+   inform the player how many fruits  +")
-  print("+   they got right in the correct      +")
-  print("+   position, and how many fruits were +")
-  print("+   correct but in the wrong position. +")
-  print("+                                      +")
-  print("+ 4)Be aware that the game can select a+")
-  print("+   fruit more than once.              +")
-  print("+                                      +")
-  print("+ 5)The aim of this game is to find the+")
-  print("+   correct sequence with the least    +")
-  print("+   guesses. That's all, please enjoy! +")
-  print("++++++++++++++++++++++++++++++++++++++++")
-  print("")
-
-#hint text displayed after every guess to provide feedback to user
-def hint():
-  print("  apple | banana | orange | lemon  ")
-  print("Guess number: ", gcount)
-  print("Correct fruits in the right position: ", correct)
-  print("Correct fruits in the wrong position: ", wrongpos)
-  print(" ")
-
-#text printed out when user has guessed all 4 fruits correctly
-def win():
-  print("           Congratulations! You have won the game!")
-  print("    The answer was: ", ansfruit)
-  print("                  You won in",gcount, " guesses!")
-  print("")
+    
+colors = ['RED', 'ORANGE', 'YELLOW', 'GREEN', 'BLUE', 'PURPLE']
+colors_map = {1:'RED', 2:'ORANGE', 3:'YELLOW', 4:'GREEN', 5:'BLUE', 6:'PURPLE'} #use codes to represent the colors
+guess_colors = ['', '', '', ''] #to change the input integers into the color strings
+chances = 10 #users only have 10 chances
+turn = 0 #keep track of how many times the users guessed
+cont = 0 #flag to stop the loop
+passcode = random.choices(colors, k=4) #to select 4 colors at random
 
 
-intro()
-#starting data
-availfruits = ["apple", "banana", "orange", "lemon"]
-ansfruit = random.choices(availfruits, k=4)
-guessfruit = ["", "", "", ""]
-gcount = 0
-game = True
-print(ansfruit)
-while game == True:
-    #asks for inputs and splits the inputs up
-         guess = list(map(str, input("Enter your guess of fruits: ").split()))
-    #checks if there are too many or not enough inputs
-         if len(guess) != 4:
-          print("Please guess 4 fruits with space in between")
-          print("  apple | banana | orange | lemon  ")
-          print("")
-          continue
-        #checks whether the inputs are from the availfruits list
-         inrange = 0
-         for x in guess:
-           if x not in availfruits:
-             inrange = 1
-#display error message if input is not one of the four fruits
-         if inrange == 1:
-           print("Please only guess fruits in the list.")
-           print("  apple | banana | orange | lemon  ")
-           print(" ")
-           continue
-        #changes inputs to string for examination
-         for y in range(4):
-           guessfruit[y] = str(guess[y])
-
-         print(guessfruit)
-
-         correct = 0
-         wrongpos = 0
-    #checks whether fruit is correct and in correct position
-         for y in range(4):
-           if guessfruit[y] == ansfruit[y]:
-             correct = correct + 1
-           else:
-               #checks whether fruit is correct but in wrong position
-             for z in guessfruit:
-               if z == ansfruit[y] and z != ansfruit[guessfruit.index(z)]:
-                 wrongpos = wrongpos + 1
-                    #increases guess count to show guess count to user
-         gcount = gcount + 1
-         hint()
-    #checks whether the user win by having all four correct fruits in correct position
-         if correct == 4:
-           game = False
-           win()
-           endscreen = "d"
-           #puts game into false mode so it loop closes and prompts user whether they want to play again
-           while game == False:
-               endscreen = input("   Do you want to play again? Type Yes(Y/y) or No(N/n): ").lower()
-               print("")
-               #if user types N or n msg will be displayed and program will end
-               if endscreen == "n":
-                   print("Thank you for playing this game! Feel free to play again next time!")
-                   print("")
-                   exit()
-                   #if user types Y or y msg will be displayed and game will start again, resetting guess count and generate new fruit answer
-               elif endscreen == "y":
-                   intro()
-                   gcount = 0
-                   ansfruit = random.choices(availfruits, k=4)
-                   print(ansfruit)
-                   game = True
+def print_start(): #game heaading
+    print("-----------------------------------------------------------")
+    print("                              MASTERMIND GAME")
+    print("-----------------------------------------------------------")
+    print("Your are required to find out the 4 colors in the passcode.")
+    print("You have 10 chances to guess.")
+    print("Here is the list of colors you may choose from:")
+    print("__________________________________________________________________________")
+    print("       RED    ORANGE    YELLOW    GREEN    BLUE    PURPLE")
+    print("__________________________________________________________________________")
+    print("Please enter codes to represent the colors as below:")
+    print("1:'RED', 2:'ORANGE', 3:'YELLOW', 4:'GREEN', 5:'BLUE', 6:'PURPLE'")
+    print("Example: YELLOW RED BLUE PURPLE --> 3 1 5 6")
+    print(" ")
+    print("NOTE: COLORS CAN BE REPREATED IN THE PASSCODE" )
+    print("__________________________________________________________________________")
 
 
+def show_hint(): #function to tell users the number of correct colors
+    print("Turn: " , turn , "/10")
+    print("Correct colours in the right position: ", correct)
+    print("Correct colours in the wrong position: ", wrng_pos)
+    print(" ")
+    print("__________________________________________________________________________")
+
+
+def win(): #function to print out after winning
+    print("           CONGRATUALTIONS! YOU GOT IT RIGHT!")
+    print("    The answer was: ", passcode)
+    print("             You got it right in", turn, " tries!")
+    print("__________________________________________________________________________")
+
+
+def lose(): #function to print out after finishing the chances
+    print("            OH NO! YOU HAVE LOST THE GAME")
+    print("    The answer was:" , passcode)
+    print("                    Better luck next time!")
+    print("___________________________________________________________________________")
+    
+
+    
+print_start()
+while cont == 0:
+    try:
+        guess = list(map(int, input("Enter your choice of colours: ").split()))
+    except ValueError: #when users' inputs are not integers
+        print("PLEASE ONLY ENTER CODES TO REPRESENT THE COLORS")
+        print(" ")
+        continue
+    
+    if len(guess)!= 4: #when user enters lesser or more than 4 codes
+        print("PLEASE ENTER 4 CODES WITH SPACE IN BETWEEN")
+        print(" ")
+        continue
+    
+    in_range = 0 #a flag to detect if the codes entered are in the list
+    for x in guess:
+        if x<1 or x>7:
+            in_range = 1
+            
+    if in_range == 1: 
+        print("PLEASE ONLY ENTER CODES FROM THE LIST")
+        print(" ")
+        continue
+
+
+    for i in range(4):
+        guess_colors[i] = colors_map[guess[i]]  #change code into color string
+
+    print(guess_colors)
+    
+    correct = 0
+    wrng_pos = 0 
+    
+    for i in range(4):
+        if guess_colors[i] == passcode[i]: #correct color in correct position
+            correct += 1
+        else:
+            for j in guess_colors:
+                if j == passcode[i] and j != passcode[guess_colors.index(j)]: #correct color in wrong position
+                    wrng_pos += 1
+
+    turn += 1 #increase turn everytime they guess
+    show_hint()
+
+
+    if correct == 4: #when users get all 4 correct they win
+        cont = 1
+        win()
+
+    if turn == chances: #when users used up all the tries
+        cont = 1
+        lose()
+       
+        
+        
+
+    
